a16591f3e45fc6ee8ebc6ea05b892779f43a23d3
lby012345 <libygz@163.com>
2018-06-12 10:14:22 +0800

    Initial Commit

---
+++ NoteProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoteProject
+//
+//  Created by liby on 2018/6/12.
+//  Copyright © 2018年 liby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NoteProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NoteProject
+//
+//  Created by liby on 2018/6/12.
+//  Copyright © 2018年 liby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
